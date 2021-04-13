75ebe634dfdc8534f7027883049bb80ed463905a
avelarcesar <82474648+avelarcesar@users.noreply.github.com>
2021-04-13 17:25:42 -0300

    project-v0.1

---
+++ navbar.php
@@ -0,0 +1,24 @@
+<div class="container-fluid p-0">
+	<section class="nav-midia color_nav-midia">
+		<a class="facebook-icon" href= "#"></a>
+		<a class="instagram-icon" href= "#"></a>
+		<a class="whatsapp-icon" href= "#"></a>
+		<a class="youtube-icon" href= "#"></a>
+		<a class="twitter-icon" href= "#"></a>
+	</section>
+	<div class="navjc">
+		<a class="iconnav" href="#"><img class="jc-icon" src="assets/files/jc-icon.png" alt="JC Triciclos Agrícolas"></a>
+		<nav class="navbarjc">
+			<ul class="navitens">
+				<li class="navitem"><a class="navitem" href="#">PRODUTOS</a></li>
+				<li class="navitem"><a class="navitem" href="#">BLOG</a></li>				
+				<li class="navitem"><a class="navitem" href="#">REVENDAS</a></li>
+				<li class="navitem"><a class="navitem" href="#">SOBRE NÓS</a></li>
+				<li class="navitem"><a class="navitem" href="#">CONTATO</a></li>
+			</ul>
+		</nav>
+	</div>
+</div>
+	
+
+	
